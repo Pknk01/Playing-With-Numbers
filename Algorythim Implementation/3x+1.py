b0e39ff0d3aefa0fd92677899be4a5bc709da1a6
Victor Ferrara <N/A>
2022-05-14 17:21:44 -0500

    3x+1 changes

--- Algorythim Implementation/3x+1.py
+++ Algorythim Implementation/3x+1.py
@@ -3,57 +3,70 @@
 import matplotlib.pyplot as PLT
 import matplotlib.pylab as pylab
 
+# --- Values ---
+
+Iterations = 10
+StepIterations = bool(input("\nDo you wish to step through each iteration?\nY/N \n").upper() == "Y")
+
+PeakValueArray = []
+LengthValueArray = []
+
 # --- Plot Window ---
 pylab.gcf().canvas.manager.set_window_title("3x+1 Visualizer") #Sets title of graph window
 
-def PlotWindow(maxvalue, valuecount, array):
-    PLT.suptitle(f"Peak Value: {maxvalue} | Value Count: {valuecount}")
+def PlotWindow(maxvalue, valuecount, array, iteration):
+    PLT.suptitle(f"Iteration Peak: {maxvalue} | Iteration Length: {valuecount} | Iteration Number {iteration}")
     PLT.cla()
     PLT.plot(array, 'ro')
     PLT.plot(array)
     PLT.pause(0.01)
 
-# --- Values ---
+# --- Algorythm Function for cleanliness ---
 
-Iterations = 10
+def ThreeXPlusOne(input):
+    
+    currval = input
+    array = []
+
+    while(currval != 1):
+        
+        if(currval % 2 == 0):   #Divides by two if value is even
+            currval /= 2
+        else:                   # 3x + 1
+            currval = (currval * 3) + 1
+
+        array.append(currval) #stores to value array
+        
+        maxval = max(array)
+        count = len(array)
+        
+        PlotWindow(maxval, count, array, input) #Plots window with current values
+
+    # --- keeping values in global scope ---
+    global PeakValueArray, LengthValueArray
+    PeakValueArray.append(maxval)
+    LengthValueArray.append(count)
 
 # --- Main while loop ---
 
-while (Iterations > 0):
+while (Iterations != 1):
     
-    CurrentValue = Iterations   # starts loop
-    Maxvalues = []              # max value array
-    Valuecounts = []            # Array of how many digits are per iteration
-    Valuearray = []             # Array of 3x+1 values, resets per iteration
+    ThreeXPlusOne(Iterations)       #Runs algorythm unitll reaching 1 for Input number
 
-    while (CurrentValue > 1):
-        
-        if(CurrentValue % 2 == 0):                      # if even divide by two
-            CurrentValue /= 2
-        else:                                           # if odd, multiply by 3 add 1
-            CurrentValue = (CurrentValue * 3) + 1
+    if(StepIterations):             #waits for a buttonpress before iterating on the next value
+        PLT.waitforbuttonpress()
 
-        Valuearray.append(CurrentValue)                 # Appends values of this tteration
-    
-    Maxvalue.append()
-    
-        
-
-
-
-while (CurrentValue > 1):
-
-    if(CurrentValue % 2 == 0): 
-        CurrentValue /= 2 
-    else:
-        CurrentValue = (CurrentValue * 3) + 1
-    
-    ValueArray.append(CurrentValue)
-    maxvalue = max(ValueArray)
-    valuecount = len(ValueArray)
-
-
+    Iterations -= 1                 #decreases index of iterations and loops
 
 #--- output ---
 
-PLT.show() #keeps the window from closing after finishing the looping
+pylab.gcf().canvas.manager.set_window_title("Peak Length Values & Length Values vs Iteration Index") #Sets title of graph window
+PLT.suptitle(f"All-Time Peak Value : {max(PeakValueArray)} | All-time longest iteration {max(LengthValueArray)}\n Red = Length Values | Blue = Peak Values")
+PLT.xlabel("Array Data")
+PLT.ylabel("Iteration Index")
+
+PLT.scatter(PeakValueArray, range(1, len(PeakValueArray) + 1), color='blue')
+PLT.scatter(LengthValueArray, range(1, len(LengthValueArray) + 1), color='red')
+
+PLT.pause(0.01) #updates window one last time
+PLT.show()      #keeps the window from closing after finishing the looping
