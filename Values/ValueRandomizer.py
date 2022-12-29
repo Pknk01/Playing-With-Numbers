8f997468893f5eedda339d865744bf9a84e5e592
Victor Ferrara <N/A>
2022-12-29 04:42:25 -0500

    Typo Fixes and Bug Fixes

--- Values/ValueRandomizer.py
+++ Values/ValueRandomizer.py
@@ -3,12 +3,12 @@ import Utilities as utilities
 
 file_path = utilities.RawValPath  #Value Filepath
 
-minval = 1
-maxval = 10
+min_val = 1
+max_val = 10
 
 with open(file_path, 'w') as file:
 
-    val = list(range(minval, maxval + 1))
+    val = list(range(min_val, max_val + 1))
     shuffle(val)
 
     file.writelines(["{}\n".format(i) for i in val])
